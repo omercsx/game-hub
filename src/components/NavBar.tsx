9958c28763f91c5afcfbc98044d707e9f656e5bb
omercsx <omercagris@gmail.com>
2024-08-01 19:17:45 -0400

    add color mode switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,14 @@
-import { HStack, Image, Text } from '@chakra-ui/react';
+import { HStack, Image } from '@chakra-ui/react';
+import ColorModeSwitch from './ColorModeSwitch';
 
 const NavBar = () => {
 	return (
-		<HStack>
+		<HStack justifyContent='space-between' padding='10px'>
 			<Image
 				src='https://omercsx.s3.ca-central-1.amazonaws.com/frontend-projects/gamehub/Gamehub_logo.svg'
 				boxSize='80px'
 			/>
-			<Text>NavBar</Text>
+			<ColorModeSwitch />
 		</HStack>
 	);
 };
