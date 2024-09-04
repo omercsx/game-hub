5c6fb1956a3158f86bbc065917ba1616bf8d2ca9
omercsx <omercagris@gmail.com>
2024-09-04 10:37:55 -0400

    Search Input component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,15 @@
 import { HStack, Image } from '@chakra-ui/react';
 import ColorModeSwitch from './ColorModeSwitch';
+import SearchInput from './SearchInput';
 
 const NavBar = () => {
 	return (
-		<HStack justifyContent='space-between' padding='10px'>
+		<HStack padding='10px'>
 			<Image
 				src='https://omercsx.s3.ca-central-1.amazonaws.com/frontend-projects/gamehub/Gamehub_logo.svg'
 				boxSize='80px'
 			/>
+			<SearchInput />
 			<ColorModeSwitch />
 		</HStack>
 	);
