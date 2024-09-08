ec2d51ebb7cba6f19cc7444acd7bc3502fb87ca3
omercsx <omercagris@gmail.com>
2024-09-08 09:46:20 -0400

    refactor - reusable API Client

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,7 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import type { GameQuery } from '../App';
-import { type FetchResponse } from '../services/api-client';
-import apiClient from '../services/api-client';
+import APIClient, { type FetchResponse } from '../services/api-client';
 import type { Platform } from './usePlatforms';
 
 export interface Game {
@@ -13,20 +12,20 @@ export interface Game {
 	rating_top: number;
 }
 
+const apiClient = new APIClient<Game>('/games');
+
 const useGames = (gameQuery: GameQuery) =>
 	useQuery<FetchResponse<Game>, Error>({
 		queryKey: ['games', gameQuery],
 		queryFn: () =>
-			apiClient
-				.get<FetchResponse<Game>>('/games', {
-					params: {
-						genres: gameQuery.genre?.id,
-						parent_platform: gameQuery.platform?.id,
-						ordering: gameQuery.sortOrder,
-						search: gameQuery.searchText,
-					},
-				})
-				.then(res => res.data),
+			apiClient.getAll({
+				params: {
+					genres: gameQuery.genre?.id,
+					parent_platform: gameQuery.platform?.id,
+					ordering: gameQuery.sortOrder,
+					search: gameQuery.searchText,
+				},
+			}),
 	});
 
 export default useGames;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,13 +1,29 @@
-import axios from 'axios';
+import axios, { type AxiosRequestConfig } from 'axios';
 
-export default axios.create({
+export interface FetchResponse<T> {
+	count: number;
+	results: T[];
+}
+
+const axiosInstance = axios.create({
 	baseURL: 'https://api.rawg.io/api',
 	params: {
 		key: '5b81286106bd4b0f9a93b0c025a3638e',
 	},
 });
 
-export interface FetchResponse<T> {
-	count: number;
-	results: T[]
+class APIClient<T> {
+	endpoint: string;
+
+	constructor(endpoint: string) {
+		this.endpoint = endpoint;
+	}
+
+	getAll = (config: AxiosRequestConfig) => {
+		return axiosInstance
+			.get<FetchResponse<T>>(this.endpoint, config)
+			.then(res => res.data);
+	};
 }
+
+export default APIClient;
